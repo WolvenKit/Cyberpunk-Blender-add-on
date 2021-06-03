28032b95a5adc607df52bc6bd8a7c9c032f4dd22
HitmanHimself <abhin7696@gmail.com>
2021-06-04 03:42:17 +0530

    updates for red4cr2w and some opusinfo read

---
+++ WolvenKit/test/Functions/OpusTools.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolvenKit.Modkit.RED4.Opus
+{
+    class OpusTools
+    {
+        public static void Parse()
+        {
+
+        }
+    }
+    class OpusInfo
+    {
+                                    //  S       N       D   ?....
+        public byte[] Header { get; } = { 0x53, 0x4E, 0x44, 0x20, 0x02, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x00 };
+        public UInt32 OpusCount { get; set; }
+        public UInt32 GroupingObjSize4x { get; set; }
+        public UInt32[] OpusHashes { get; set; }
+        public UInt16[] PackIndices { get; set; }
+        public UInt32[] OpusOffsets { get; set; }
+        public UInt16[] RiffOpusOffsets { get; set; }
+        public UInt32[] OpusStreamLengths { get; set; }
+        public UInt32[] WavStreamLengths { get; set; }
+        public List<Group> GroupObjs { get; set; }
+        public class Group
+        {
+            public UInt32 Hash { get; set; }
+            public UInt32 MemberCount { get; set; }
+            public UInt32[] MemberHashes { get; set; }
+        }
+        public OpusInfo(MemoryStream ms)
+        {
+            BinaryReader br = new BinaryReader(ms);
+            OpusCount = br.ReadUInt32();
+            GroupingObjSize4x = br.ReadUInt32();
+
+            OpusHashes = new UInt32[OpusCount];
+            for(int i = 0; i < OpusCount; i++)
+            {
+                OpusHashes[i] = br.ReadUInt32();
+            }
+
+            PackIndices = new UInt16[OpusCount];
+            for (int i = 0; i < OpusCount; i++)
+            {
+                PackIndices[i] = br.ReadUInt16();
+            }
+
+            OpusOffsets = new UInt32[OpusCount];
+            for (int i = 0; i < OpusCount; i++)
+            {
+                OpusOffsets[i] = br.ReadUInt32();
+            }
+
+            RiffOpusOffsets = new UInt16[OpusCount];
+            for (int i = 0; i < OpusCount; i++)
+            {
+                RiffOpusOffsets[i] = br.ReadUInt16();
+            }
+
+            OpusStreamLengths = new UInt32[OpusCount];
+            for (int i = 0; i < OpusCount; i++)
+            {
+                OpusStreamLengths[i] = br.ReadUInt32();
+            }
+
+            WavStreamLengths = new UInt32[OpusCount];
+            for (int i = 0; i < OpusCount; i++)
+            {
+                WavStreamLengths[i] = br.ReadUInt32();
+            }
+            GroupObjs = new List<Group>();
+            while (ms.Position < ms.Length)
+            {
+                Group group = new Group();
+                group.Hash = br.ReadUInt32();
+                group.MemberCount = br.ReadUInt32();
+                group.MemberHashes = new UInt32[group.MemberCount];
+                for(UInt32 i = 0; i < group.MemberCount; i++)
+                {
+                    group.MemberHashes[i] = br.ReadUInt32();
+                }
+                GroupObjs.Add(group);
+            }
+        }
+    }
+}
--- WolvenKit/test/Program.cs
+++ WolvenKit/test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.IO;
 using Catel.IoC;
 using WolvenKit.Common.Services;
@@ -6,6 +7,10 @@ using WolvenKit.RED4.CR2W;
 using CP77.CR2W;
 using WolvenKit.Modkit.RED4.Materials;
 using WolvenKit.Common.Tools.Oodle;
+using WolvenKit.Modkit.RED4.Opus;
+using System.Collections.Generic;
+using WolvenKit.Common.FNV1A;
+
 namespace GLTFNodesTest
 {
     class Program
@@ -21,12 +26,67 @@ namespace GLTFNodesTest
             ServiceLocator.Default.RegisterType<IWolvenkitFileService, Cp77FileService>();
             ServiceLocator.Default.RegisterType<ModTools>();
 
-            string g = @"C:\Program Files (x86)\Steam\steamapps\common\Cyberpunk 2077\archive\pc\content";
-            string f = @"C:\Users\Abhinav\Desktop\t1_001_wa_tank__judy.mesh";
-            MATERIAL m = new MATERIAL(new DirectoryInfo(g));
+            /*
+            string g = @"C:\Users\Abhinav\Desktop\missinghashes.txt";
+            string[] miss = File.ReadAllLines(g);
+            UInt64[] missed = new UInt64[miss.Length];
+            for(int i = 0; i < miss.Length; i++)
+            {
+                missed[i] = Convert.ToUInt64(miss[i]);
+            }
+            */
+            string f = @"C:\Users\Abhinav\Desktop\cp_list.txt";
+            string[] lines = File.ReadAllLines(f);
+            List<string> files = new List<string>();
+            for(int i = 20; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Substring(53, lines[i].Length - 53);
+                files.Add(lines[i].Replace('/', '\\'));
+                /*
+                if(lines[i].Contains("/base/"))
+                {
+                    lines[i] = lines[i].Substring(lines[i].IndexOf("/base/") + 1, lines[i].Length - lines[i].IndexOf("/base/") - 1);
+                    files.Add(lines[i].Replace('/','\\'));
+                }
+                */
+                //Console.WriteLine(lines[i]);
+            }
+            var dirs = Directory.GetDirectories(@"D:\unbundle", "", SearchOption.AllDirectories);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                dirs[i] = dirs[i].Replace("D:\\unbundle\\",string.Empty);
+                bool found = false;
+                foreach (string file in files)
+                {
+                    if(file.Contains(dirs[i]))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found)
+                {
+                    Console.WriteLine(dirs[i]);
+                }
+            }
 
-            m.ExportMeshWithMaterialsUsingArchives(new FileStream(f, FileMode.Open, FileAccess.Read), Path.GetFileNameWithoutExtension(f), new FileInfo(f), false, WolvenKit.Common.DDS.EUncookExtension.tga);
-            //m.ExportMeshWithMaterialsUsingAssetLib(new FileStream(f, FileMode.Open, FileAccess.Read),new DirectoryInfo(@"D:\Material Repo"), Path.GetFileNameWithoutExtension(f), new FileInfo(f));
+            /*
+            string z = @"C:\Users\Abhinav\Desktop\archivehashes.txt";
+            var ss = File.ReadAllLines(z).ToList();
+            List<string> found = new List<string>();
+            foreach (string file in files)
+            {
+                for (int i = 0; i < missed.Length; i++)
+                {
+                    if (FNV1A64HashAlgorithm.HashString(file) == missed[i])
+                    {
+                        found.Add(file);
+                    }
+                }
+
+            }
+            File.WriteAllLines(@"C:\Users\Abhinav\Desktop\ahsha.txt", found);
+            */
         }
     }
 }
