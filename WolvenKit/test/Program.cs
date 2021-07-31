599083e94e1dd98ff1090e40df24ca08bec5b0fc
HitmanHimself <76775663+HitmanHimself@users.noreply.github.com>
2021-07-31 21:35:26 +0530

    ree

--- WolvenKit/test/Program.cs
+++ WolvenKit/test/Program.cs
@@ -5,12 +5,8 @@ using WolvenKit.Common.Services;
 using WolvenKit.RED4.CR2W;
 using CP77.CR2W;
 using WolvenKit.Common.Tools.Oodle;
-using WolvenKit.Modkit.RED4.MeshFile;
 using WolvenKit.Modkit.RED4;
 using WolvenKit.Modkit.RED4.RigFile;
-using System.Collections.Generic;
-using WolvenKit.RED4.CR2W.Archive;
-using WolvenKit.Modkit.RED4.Materials;
 
 namespace TEST
 {
@@ -19,18 +15,14 @@ namespace TEST
         static void Main(string[] args)
         {
             var serviceLocator = ServiceLocator.Default;
-            serviceLocator.RegisterType<ILoggerService, CatelLoggerService>();
             serviceLocator.RegisterType<IProgress<double>, PercentProgressService>();
             serviceLocator.RegisterType<IHashService, HashService>();
             serviceLocator.RegisterType<Red4ParserService>();
-            serviceLocator.RegisterType<TargetTools>();
             serviceLocator.RegisterType<RIG>();
             serviceLocator.RegisterType<MeshTools>();
-            serviceLocator.RegisterType<MESHIMPORTER>();
             serviceLocator.RegisterType<ModTools>();
             var oodlePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "oo2ext_7_win64.dll");
             OodleLoadLib.Load(oodlePath);
-
         }
     }
 }
