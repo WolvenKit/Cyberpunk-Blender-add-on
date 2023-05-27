f5d672b22dbf0b0736608c3a7dda5a6a38c4c805
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2023-05-27 02:39:10 -0400

    Update exporters.py
    better formatting for default settings, added a check for UVs and vert counts to ensure the meshes will import back into wolvenkit

--- i_scene_cp77_gltf/main/exporters.py
+++ i_scene_cp77_gltf/main/exporters.py
@@ -10,35 +10,30 @@ def default_cp77_options():
         'export_yup': True,
         'export_animations': True,
         'export_tangents': True,
-        'export_morph': True,
-        'export_morph_normal': True,
-        'export_morph_tangent': True,
         'export_materials': 'NONE',
         'export_all_influences': True,
         'export_lights': False,
+        'export_morph_tangent': True,
+        'export_morph_normal': True,
+        'export_apply': False
+    }
+    return options
+
+def cp77_mesh_options():
+    options = {
+        'export_morph': True,
+        'export_morph_normal': True,
+        'export_all_influences': True,
         'export_colors': True
     }
     return options
 
 def pose_export_options():
     options = {
-        'export_format': 'GLB',
-        'check_existing': True,
-        'export_skins': True,
-        'export_cameras': False,
-        'export_yup': True,
         'export_frame_range': False,
-        'export_animations': True,
-        'export_all_influences': True,
-        'export_anim_single_armature': True,
-        'export_apply': False,
-        'export_tangents': True,
-        'export_materials': 'NONE',
-        'export_lights': False,
-        'export_morph_normal': True
+        'export_anim_single_armature': True       
     }
     return options
-
     
 def export_cyberpunk_glb(context, filepath, export_poses):
     # Retrieve the selected objects
@@ -50,18 +45,28 @@ def export_cyberpunk_glb(context, filepath, export_poses):
             bpy.ops.cp77.message_box('INVOKE_DEFAULT', message="No armature selected, please select an armature")
             return {'CANCELLED'}
         #if the export poses value is True, set the export options to ensure the armature is exported properly with the animations
-        options = pose_export_options()
+        options = default_cp77_options()
+        options.update(pose_export_options())
     else:
     #if export_poses option isn't used, check to make sure there are meshes selected and throw an error if not
         meshes = [obj for obj in objects if obj.type == 'MESH']
         if not meshes:
             bpy.ops.cp77.message_box('INVOKE_DEFAULT', message="No meshes selected, please select at least one mesh")
             return {'CANCELLED'}
+    #check that meshes include UVs and have less than 65000 verts, throw an error if not
+        for mesh in meshes:
+            if not mesh.data.uv_layers:
+                bpy.ops.cp77.message_box('INVOKE_DEFAULT', message="Meshes must have UV layers in order to import in Wolvenkit")
+                return {'CANCELLED'}
+            for submesh in mesh.data.polygons:
+                if len(submesh.vertices) > 65000:
+                    bpy.ops.cp77.message_box('INVOKE_DEFAULT', message="Each submesh must have less than 65,000 vertices")
+                    return {'CANCELLED'}
         #use the right options for exporting a mesh
         options = default_cp77_options()
+        options.update(cp77_mesh_options())
 
     print(options)  
-
     # if exporting meshes, make sure that any hidden armatures connected to those meshes are exported by making them visible, selecting them
     hidden_armatures = []
     for obj in objects:
@@ -80,4 +85,4 @@ def export_cyberpunk_glb(context, filepath, export_poses):
     for armature in hidden_armatures:
         armature.select_set(False)  # Deselect the armature
         armature.hide_set(True)  # Hide the armature
-  # Report success               
+          
