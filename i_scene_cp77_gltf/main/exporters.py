6e666d7698597eb8b4abea7ef52dafbe681c797d
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2023-09-07 02:58:43 -0400

    Update exporters.py
    hair profile export

--- i_scene_cp77_gltf/main/exporters.py
+++ i_scene_cp77_gltf/main/exporters.py
@@ -172,3 +172,90 @@ def cp77_collision_export(filepath):
             json.dump(data, f, indent=2)
 
     print(f'Finished exporting {new_file_name}')
+
+def cp77_hp_export(filepath):
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    template_relative_path = os.path.join('..', 'resources', 'hair_profile_template.hp.json')
+
+    # Get the absolute path to the template file
+    template_hp_file = os.path.normpath(os.path.join(script_directory, template_relative_path))
+
+    obj = bpy.context.selected_objects[0]
+
+    # Detect the active material slot
+    active_slot = obj.active_material_index
+
+    if active_slot is None:
+        print("No active material slot found.")
+    else:
+        mat = obj.active_material
+
+        # Load the template hair profile JSON
+        with open(template_hp_file, 'r') as template_f:
+            j = json.load(template_f)
+
+        crs = ['Color Ramp', 'Color Ramp.001']
+
+        nodes = mat.node_tree.nodes
+        print(' ')
+        print('Gradient Stops for', mat.name)
+
+        for crname in crs:
+            cr = nodes.get(crname)
+            if cr:
+                print(' ')
+                print('Stop info for', cr.label)
+                grad = ''
+                if cr.label == 'GradientEntriesRootToTip':
+                    grad = 'gradientEntriesRootToTip'
+                elif cr.label == 'GradientEntriesID':
+                    grad = 'gradientEntriesID'
+
+                j_data = j['Data']['RootChunk'][grad]
+                elements = list(cr.color_ramp.elements)
+                elements.reverse()
+
+                for i, stop in enumerate(elements):
+                    print('Stop #', i)
+                    print('Position -', stop.position)
+                    print('Color -', int(stop.color[0] * 255), int(stop.color[1] * 255), int(stop.color[2] * 255))
+                    print(' ')
+
+                    # Create a new entry for each stop
+                    new_entry = {
+                        "$type": "rendGradientEntry",
+                        "color": {
+                            "$type": "Color",
+                            "Alpha": int(stop.color[3] * 255),
+                            "Blue": int(stop.color[2] * 255),
+                            "Green": int(stop.color[1] * 255),
+                            "Red": int(stop.color[0] * 255),
+                        },
+                        "value": stop.position,
+                    }
+
+                    j_data.append(new_entry)
+
+        # Construct the output path
+        outpath = os.path.join(filepath, f'{mat.name}.hp.json')
+
+        if not os.path.exists(os.path.dirname(outpath)):
+            os.makedirs(os.path.dirname(outpath))
+
+        # Save the updated JSON to the specified output path
+        with open(outpath, 'w') as outfile:
+            json.dump(j, outfile, indent=2)
+
+# def ExportAll(self, context):
+# # Iterate through all objects in the scene
+#     for obj in bpy.context.scene.objects:
+#         if obj.type == 'MESH':
+#             # Check if the object has a sourcePath property
+#             if 'sourcePath' in obj:
+#                 source_path = obj['sourcePath']
+#                 # Set the active object to the current mesh
+#                 bpy.context.view_layer.objects.active = obj
+#                 # Export the meshes
+#                 bpy.ops.CP77GLBExport(filepath=source_path)
+
