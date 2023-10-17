b656926201faf114a8b33047681fd5d022c9cc7c
Simarilius <simarilius@yahoo.com>
2023-10-17 18:07:10 +0100

    Fix to the fix on entirys not being grouped in sector import, previous 'fix' totally broke things.

--- i_scene_cp77_gltf/importers/sector_import.py
+++ i_scene_cp77_gltf/importers/sector_import.py
@@ -24,7 +24,7 @@ from pathlib import Path
 import time
 import traceback
 from pprint import pprint 
-from .setup import MaterialBuilder
+from ..main.setup import MaterialBuilder
 
 scale_factor=1
 
@@ -296,14 +296,8 @@ def importSectors( filepath='', want_collisions=False, am_modding=False, with_ma
                     else:
                         try:
                             print('Importing ',entpath, ' using app ',app)
-                            bpy.ops.io_scene_gltf.cp77entity(filepath=entpath, appearances=app,with_materials=with_materials)
-                            objs = C.selected_objects
-                            move_coll= bpy.data.collections.get( objs[0].users_collection[0].name )
-                            top_col=[col for col in C.scene.collection.children if col.user_of_id(move_coll)][0]
-                            if top_col:
-                                Masters.children.link(top_col) 
-                                coll_scene.children.unlink(top_col)
-                                imported=True
+                            bpy.ops.io_scene_gltf.cp77entity(filepath=entpath, appearances=app,with_materials=with_materials, inColl='Masters')
+                            imported=True
                         except:
                             print(traceback.print_exc())
                             print('Failed during Entity import on ',os.path.basename(entpath))
