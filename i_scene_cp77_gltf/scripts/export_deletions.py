4cc4afe73d91410dc90148ff74d34bff2a35cc90
manavortex <manavortex@gmail.com>
2023-11-10 22:50:56 +0100

    Update export_deletions.py
    will no longer export nodes multiple times if there are several meshes under the same node
    will use sector path for target path, rather than guessing from file name

--- i_scene_cp77_gltf/scripts/export_deletions.py
+++ i_scene_cp77_gltf/scripts/export_deletions.py
@@ -1,40 +1,115 @@
 import bpy
+import re
+
+wolvenkit_project = "apartment_dogtown_cleaned_up"
+mod_directory = "C:\\Games\\Cyberpunk 2077\\archive\\pc\\mod\\"
+project_directory = "F:\\CyberpunkFiles\\world_editing\\"
+
+export_to_mod_dir = True
+consider_partial_deletions = True
+
+# Specify the filename where you want to save the output. Make sure to use an existing folder!
+output_filename = f"{project_directory}\\{wolvenkit_project}\\source\\resources\\{wolvenkit_project}.archive.xl"
+
+if export_to_mod_dir:
+    output_filename = f"{mod_directory}\\{wolvenkit_project}.archive.xl"
+
+# if an item matches all strings in one of the sub-arrays, delete it
+delete_partials = [
+    [ "soda_can" ],
+    [ "squat_clothes" ],
+    [ "takeout_cup" ],
+    [ "trash" ],
+]
+
+# if an item matches all strings in one of the sub-arrays, keep it. Supports regular expression.
+keep_partials = [ 
+    [ "^q\d\d" ],
+    # ["entropy_lamp.*"]
+]
+
+
+# For indenting your .xl file
+indent="  "
+
+# --------------------------- DO NOT EDIT BELOW THIS LINE -------------------------------------
 
 deletions = {}
 expectedNodes = {}
 
-indent="  "
-
 # function to recursively count nested collections
 def countChildNodes(collection):
-    numChildNodes = len(coll.children_recursive)
-    return numChildNodes
+    if 'expectedNodes' in collection:
+        numChildNodes = collection['expectedNodes']
+        return numChildNodes
 
-# Function to recursively find collections without children (these contain deletions)
+
+# Compile regular expressions for keep_partials
+compiled_partials = [[re.compile(p) for p in partials] for partials in keep_partials]
+
+# Function to find collections without children (these contain deletions)
 def find_empty_collections(collection):
-    coll = collection
-    empty_collections =[col for col in coll.children_recursive if len(col.children) == 0 and len(col.objects) == 0 and 'nodeIndex' in col and 'nodeType' in col]
+    empty_collections = []
+    is_deletion_candidate = 'nodeIndex' in collection and 'nodeType' in collection
+
+    # check if we want to keep this collection
+    for keep_check in compiled_partials:
+        if all(p.search(collection.name) for p in keep_check):
+            return empty_collections
+
+    if len(collection.children) == 0 and is_deletion_candidate:
+        if len(collection.objects) == 0:
+            empty_collections.append(collection)
+        if consider_partial_deletions and len(collection.children) > 0 and not collection.children[0]["Name"].startswith("submesh_00"):
+            empty_collections.append(collection)
+    elif is_deletion_candidate:
+        for deletion_check in delete_partials:
+            if all(partial in collection.name for partial in deletion_check):
+                empty_collections.append(collection)
+        
+    for child_collection in collection.children:
+        empty_collections.extend(find_empty_collections(child_collection))        
+
     return empty_collections
 
-# Function to serialize to text file (OK, to console for now...)
-def to_archive_xl():
-    print("\n streaming:")
-    print(f"{indent}sectors:")
-    for sectorName in deletions:
-        print(f"{indent}{indent}- path: base\worlds\\03_night_city\_compiled\default\{sectorName}")
-        print(f"{indent}{indent}{indent}expectedNodes: {expectedNodes[sectorName]}")
-        print(f"{indent}{indent}{indent}nodeDeletions:")
-        sectorData = deletions[sectorName]
 
-        for empty_collection in sectorData:
-            print(f"{indent}{indent}{indent}{indent}# {empty_collection.name}")
-            print(f"{indent}{indent}{indent}{indent}- index: {empty_collection['nodeIndex']}")
-            print(f"{indent}{indent}{indent}{indent}{indent}type:  {empty_collection['nodeType']}")
+# Function to write to a text file
+def to_archive_xl(filename):
+    with open(filename, "w") as file:
+        file.write("streaming:\n")
+        file.write(f"{indent}sectors:\n")
+        for sectorPath in deletions:
+            deletedNodes = []
+            file.write(f"{indent}{indent}- path: {sectorPath}\n")
+            file.write(f"{indent}{indent}{indent}expectedNodes: {expectedNodes[sectorPath]}\n")
+            file.write(f"{indent}{indent}{indent}nodeDeletions:\n")
+            sectorData = deletions[sectorPath]
+
+            currentNodeIndex = -1
+            currentNodeComment = ''
+            currentNodeType = ''
+            for empty_collection in sectorData:
+                if empty_collection['nodeIndex'] > currentNodeIndex:
+                    # new node! write the old one                    
+                    file.write(f"{indent}{indent}{indent}{indent}# {currentNodeComment}\n")
+                    file.write(f"{indent}{indent}{indent}{indent}- index: {currentNodeIndex}\n")
+                    file.write(f"{indent}{indent}{indent}{indent}{indent}type: {currentNodeType}\n")
+                    
+                    # set instance variables
+                    currentNodeIndex = empty_collection['nodeIndex']
+                    currentNodeComment = empty_collection.name
+                    currentNodeType = empty_collection['nodeType']
+                elif empty_collection['nodeIndex'] == currentNodeIndex:
+                    currentNodeComment = f"{currentNodeComment}, {empty_collection.name}"
+
 
 # Iterate over matching collections and find empty ones
 for sectorCollection in [c for c in bpy.data.collections if c.name.endswith("streamingsector")]:
-    expectedNodes[sectorCollection.name] = countChildNodes(sectorCollection)
-    if expectedNodes[sectorCollection.name] > 0:
-        deletions[sectorCollection.name] = find_empty_collections(sectorCollection)
+    prefix, file_path = sectorCollection["filepath"].split('raw\\')
+    file_path = file_path.replace(".json", "")
+    expectedNodes[file_path] = countChildNodes(sectorCollection)
+    collections = find_empty_collections(sectorCollection)
+    if len(collections) > 0:
+        deletions[file_path] = collections
 
-to_archive_xl()
+to_archive_xl(output_filename)
