8fe6c58a40de9de724a05ce30f0b812cd0752a3e
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2023-12-20 16:41:20 -0500

    exposed function for easily hiding deform bones in rigs

--- i_scene_cp77_gltf/main/animtools.py
+++ i_scene_cp77_gltf/main/animtools.py
@@ -77,7 +77,7 @@ def play_anim(self, context, anim_name):
 
     if active_action:
         # Stop the currently playing animation
-        bpy.ops.screen.animation_cancel(restore_frame=False)
+        bpy.ops.screen.animation_cancel(restore_frame=True)
 
         # Set the active action
         obj.animation_data.action = active_action
@@ -96,6 +96,7 @@ def delete_anim(self, context):
 
 def hide_extra_bones(self, context):
     # List of bone names that should not be hidden
+    selected_object = context.active_object
     ## the regular animrig bones
     animBones = ["Hips", "Spine", "Spine1", "Spine2", "Spine3", "LeftShoulder", "LeftArm", "LeftForeArm", "LeftHand", "WeaponLeft", "LeftInHandThumb", "LeftHandThumb1", "LeftHandThumb2", "LeftInHandIndex", "LeftHandIndex1", "LeftHandIndex2", "LeftHandIndex3", "LeftInHandMiddle", "LeftHandMiddle1", "LeftHandMiddle2", "LeftHandMiddle3", "LeftInHandRing", "LeftHandRing1", "LeftHandRing2", "LeftHandRing3", "LeftInHandPinky", "LeftHandPinky1", "LeftHandPinky2", "LeftHandPinky3", "RightShoulder", "RightArm", "RightForeArm", "RightHand", "WeaponRight", "RightInHandThumb", "RightHandThumb1", "RightHandThumb2", "RightInHandIndex", "RightHandIndex1", "RightHandIndex2", "RightHandIndex3", "RightInHandMiddle", "RightHandMiddle1", "RightHandMiddle2", "RightHandMiddle3", "RightInHandRing", "RightHandRing1", "RightHandRing2", "RightHandRing3", "RightInHandPinky", "RightHandPinky1", "RightHandPinky2", "RightHandPinky3", "Neck", "Neck1", "Head", "LeftEye", "RightEye", "LeftUpLeg", "LeftLeg", "LeftFoot", "LeftHeel", "LeftToeBase", "RightUpLeg", "RightLeg", "RightFoot", "RightHeel", "RightToeBase"]
 
