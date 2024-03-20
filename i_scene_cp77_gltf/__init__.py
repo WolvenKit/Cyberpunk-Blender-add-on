f0997bdcfe814629fe85b5e4f6cd1f1823663a1f
Simarilius-uk <simarilius@yahoo.com>
2024-03-20 19:07:57 +0000

    Update __init__.py
    bump version

--- i_scene_cp77_gltf/__init__.py
+++ i_scene_cp77_gltf/__init__.py
@@ -26,7 +26,7 @@ from .importers.phys_import import *
 bl_info = {
     "name": "Cyberpunk 2077 IO Suite",
     "author": "HitmanHimself, Turk, Jato, dragonzkiller, kwekmaster, glitchered, Simarilius, Doctor Presto, shotlastc, Rudolph2109, Holopointz",
-    "version": (1, 5, 3, 1),
+    "version": (1, 5, 4),
     "blender": (3, 6, 0),
     "location": "File > Import-Export",
     "description": "Import and Export WolvenKit Cyberpunk2077 gLTF models with materials, Import .streamingsector and .ent from .json",
