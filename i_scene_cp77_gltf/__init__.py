b4fc7d46d46bc7cd22b431900872e9282dd87b63
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2024-04-16 12:07:09 -0400

    add dependency install function

--- i_scene_cp77_gltf/__init__.py
+++ i_scene_cp77_gltf/__init__.py
@@ -1,3 +1,13 @@
+def install_dependency(dependency_name):
+    print(f"required package: {dependency_name} not found")
+    from pip import _internal as pip
+    print(f"Attempting to install {dependency_name}")
+    try:
+        pip.main(['install', dependency_name])
+        print(f"Successfully installed {dependency_name}")
+    except Exception as e:
+        print(f"Failed to install {dependency_name}: {e}")
+        
 import bpy
 import bpy.utils.previews
 import os
