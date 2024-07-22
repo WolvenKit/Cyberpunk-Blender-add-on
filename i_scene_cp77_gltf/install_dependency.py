5d5da3affcbd1241e28c5c201166901b5e7b3c61
DoctorPresto <111536029+DoctorPresto@users.noreply.github.com>
2024-07-21 20:22:09 -0400

    check os before trying dep install

--- i_scene_cp77_gltf/install_dependency.py
+++ i_scene_cp77_gltf/install_dependency.py
@@ -1,6 +1,14 @@
 import bpy
+import sys
+from .main.common import show_message
+
+_os = sys.platform
 
 def install_dependency(dependency_name):
+    if _os is not 'win32':
+        print(f"required package: {dependency_name} not found but the plugin is unable to install this automatically on OS other than Windows")
+        show_message(f"required package: {dependency_name} not found but the plugin is unable to install automatically on OS other than Windows")
+        return('CANCELLED')
     print(f"required package: {dependency_name} not found")
     from pip import _internal as pip
     print(f"Attempting to install {dependency_name}")
