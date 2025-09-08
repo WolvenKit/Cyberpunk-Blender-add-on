d475836626d947aa9d8965d9d90cfcb99c3b3ed8
Chase81 <ametis81@gmail.com>
2025-09-08 16:32:40 +0400

    Add GPU options fror sector import

--- i_scene_cp77_gltf/cyber_props.py
+++ i_scene_cp77_gltf/cyber_props.py
@@ -61,6 +61,8 @@ def SetCyclesRenderer(use_cycles=True, set_gi_params=False):
         for scene in bpy.data.scenes:
             scene.render.engine = 'CYCLES'
             scene.cycles.device = 'GPU'
+            system_prefs = bpy.context.preferences.system
+            system_prefs.gpu_backend = 'OPENGL'
 
         if set_gi_params:
             cycles = bpy.context.scene.cycles
@@ -71,6 +73,7 @@ def SetVulkanBackend(use_vulkan=True):
     if use_vulkan:
         system_prefs = bpy.context.preferences.system
         system_prefs.gpu_backend = 'VULKAN'
+        
 
 def CP77CollectionList(self, context):
     items = []
@@ -256,13 +259,14 @@ class CP77_PT_PanelProps(PropertyGroup):
     use_cycles: BoolProperty(
         name="Set Render Engine to Cycles",
         default=False,
-        description="Sets the Render Engine to Cycles. Imported shaders may fail to compile while using EEVEE without the Vulkan backend"
+        description="""(Requires Restart if Vulkan backend being used)  Sets the Render Engine to Cycles. Imported shaders may fail to compile while using EEVEE without the Vulkan backend.
+This setting can also be changed from within system preferences."""
     )
 
     use_vulkan: BoolProperty(
         name="Set Backend to Vulkan",
         default=True,
-        description="""(Requires Restart) Sets the Blender graphics backend to Vulkan which can compile shaders that fail using OpenGL.
+        description="""(Requires Restart if OpenGL backend being used) Sets the Blender graphics backend to Vulkan which can compile shaders that fail using OpenGL.
 This setting can also be changed from within system preferences."""
     )
 
